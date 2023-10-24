793b4fb5f93e8f4c0c4ee449663720c385bf3c93
Owen Smith <owen.smith@cloud.com>
2023-10-24 12:38:41 +0100

    Initial Commit (v9.3.1)
    Signed-off-by: Owen Smith <owen.smith@cloud.com>

---
+++ xenserver-pv-tools/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$updateUrl 	= 'https://pvupdates.vmd.citrix.com/updates.9.3.1.91.json'
+$userAgent 	= 'choco-install'
+
+$json = Invoke-RestMethod -Uri $updateUrl -UserAgent $userAgent
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  softwareName  = 'xenserver-pv-tools*'
+  url           = $json.x86.url
+  url64bit      = $json.x64.url
+  checksum      = $json.x86.checksum
+  checksum64    = $json.x64.checksum
+  checksumType  = 'sha256'
+  checksumType64= 'sha256'
+
+  # MSI
+  silentArgs    = "/qn ALLOWDRIVERINSTALL=YES /log `"$env:TEMP\chocolatey\$($packageName)\$($packageName).Install.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
